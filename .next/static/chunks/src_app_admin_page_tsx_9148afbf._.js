94d75d71b2f6a0119636472ac5dc6cb47f8a17c9
RVL <72154429+dewa11@users.noreply.github.com>
2025-06-17 23:33:58 +0800

    Add .next/static/chunks/src_app_admin_page_tsx_9148afbf._.js 1750174438134

---
+++ .next/static/chunks/src_app_admin_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e04c5fa3._.js",
+  "static/chunks/src_6d4e7f23._.js"
+],
+    source: "dynamic"
+});
